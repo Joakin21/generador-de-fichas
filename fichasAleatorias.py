5cbf0a2d0e0391a8c23ed7eb25cd9d79daeed039
Carlos <karloz.97@gmail.com>
2021-04-14 15:55:44 -0300

    Se implementa y aplica función proces_data

--- fichasAleatorias.py
+++ fichasAleatorias.py
@@ -5,39 +5,41 @@ import constant
 import json
 from pymongo import MongoClient
 from bson import ObjectId
+from cryptography.fernet import Fernet
 
 def randomDate():
     return str(barnum.create_date(past=True)).split()[0]
 
 def addField(field_data):
+    #, option
     type_field_data = field_data['tipo']
-
+    
     field_obj = {}
-    field_obj["clave"] = field_data["text"]
-    field_obj["tipo"] = 4
+    field_obj["clave"] = process_data(field_data["text"], option)
+    field_obj["tipo"] = process_data("4", option)
 
     if (type_field_data == "estructural"):
-        field_obj["valor"] = field_data["text"]
-        field_obj["tipo"] = 2
+        field_obj["valor"] = process_data(field_data["text"], option)
+        field_obj["tipo"] = process_data("2", option)
 
     elif (type_field_data == "CLUSTER"):
-        field_obj["valor"] = field_data["text"]
-        field_obj["tipo"] = 3
+        field_obj["valor"] = process_data(field_data["text"], option)
+        field_obj["tipo"] = process_data("3", option)
 
     elif ((type_field_data == "DV_CODED_TEXT" or type_field_data == "CHOICE" or type_field_data == "DV_ORDINAL") and field_data["contenido"]):
-        field_obj["valor"] = random.choice(field_data["contenido"])["text"]
+        field_obj["valor"] = process_data(random.choice(field_data["contenido"])["text"], option)
 
     elif (type_field_data == "DV_QUANTITY" or type_field_data == "DV_COUNT"):
-        field_obj["valor"] = random.randint(1,100)
+        field_obj["valor"] = process_data(str(random.randint(1,100)), option)
 
     elif (type_field_data == "DV_DATE_TIME" or type_field_data == "DV_DATE"):
-        field_obj["valor"] = randomDate()
+        field_obj["valor"] = process_data(randomDate(), option)
 
     elif (type_field_data == "DV_BOOLEAN"):
-        field_obj["valor"] = random.choice(["Yes", "No"])
+        field_obj["valor"] = process_data(random.choice(["Yes", "No"]), option)
 
     else:
-        field_obj["valor"] = barnum.create_sentence()
+        field_obj["valor"] = process_data(barnum.create_sentence(), option)
 
     archetype_for_session.append(field_obj)
 
@@ -60,37 +62,65 @@ def iterateArchetype(my_archetype):
 
             iterateArchetype(my_archetype[k])
 
+    
+def process_data(data, option):
+    if option == 1:
+        return data
+    elif option == 2:
+        return algoritmo.encrypt(data.encode("utf-8")) 
+
 client = MongoClient()
 db = client['proyecto4']
 archetype_collection = db["arquetipos"]  
+paciente_collection = db["historial_paciente"]
 
 n_collections = archetype_collection.count_documents({})
 archetype_list = archetype_collection.find({}, {"text":1})
 
+key_test = "QJhfUw_LWLPe6uEbDd808C9eUeOxUBQfj5a4ln6o8UU=".encode()
+algoritmo = Fernet(key_test)
+
 health_records = []
 n_health_records = int(input("Ingrese el número de fichas a crear: "))
 
+print("Seleccione una opción:")
+print("1. Generar archivo json")
+print("2. insertar en base de datos mongo")
+option = int(input("Opción: "))
+
+
 for ehr in range(n_health_records):
     patient_history = {}
-    patient_history["_id"] = {"$oid":str(ObjectId())}
-    patient_history["nombre"] = names.get_first_name()
-    patient_history["apellidos"] = names.get_last_name() + " " + names.get_last_name()
+    if option == 1:
+        patient_history["_id"] = {"$oid":str(ObjectId())}
+
+    patient_history["nombre"] = process_data(names.get_first_name(), option)
+    patient_history["apellidos"] = process_data(names.get_last_name() + " " + names.get_last_name(), option)
     patient_history["rut"] = str( random.randint(11111111,99999999) ) + '-' + str( random.randint(1,9) )
-    patient_history["direccion"] = barnum.create_street()
-    patient_history["fecha_nacimiento"] = randomDate()
-    patient_history["ciudad"] = random.choice(constant.CITIES)
-    patient_history["profesionales_que_atendieron"] = random.sample(constant.HC_PROFESSIONAL_ID, random.randint(1,len(constant.HC_PROFESSIONAL_ID)))#len(constant.HC_PROFESSIONAL_ID)
+    patient_history["direccion"] = process_data(barnum.create_street(), option)
+    patient_history["fecha_nacimiento"] = process_data(randomDate(), option)
+    patient_history["ciudad"] = process_data(random.choice(constant.CITIES), option)
+    patient_history["es_atendido_ahora"] = False
+    """
+    if option == 1:
+        patient_history["profesionales_que_atendieron"] = random.sample(constant.HC_PROFESSIONAL_ID, random.randint(1,len(constant.HC_PROFESSIONAL_ID)))#len(constant.HC_PROFESSIONAL_ID)
+        n_sessions = len(patient_history["profesionales_que_atendieron"])
+    if option == 2:
+        patient_history["profesionales_que_atendieron"] = [{"user_id":random.choice([117, 118]), "fecha":"07/02/2021"}]
+        n_sessions = random.randint(1,15)
+    """
+    n_sessions = random.randint(1,15)
     patient_history["sesiones_medica"] = []
 
-    n_sessions = len(patient_history["profesionales_que_atendieron"])
+    
     for i in range(n_sessions):#random.randint(1,8)
         medical_session = {}
-        medical_session["nombre_sesion"] = barnum.create_nouns()
-        medical_session["fecha"] = randomDate()
-        medical_session["nombre_profesional"] = names.get_first_name() + " " + names.get_last_name() + " " + names.get_last_name()
-        medical_session["profesion"] = random.choice(constant.HC_PROFESSIONAL_PRACTITIONERS)
-        medical_session["centro_salud"] = random.choice(constant.HOSPITALS)
-        #medical_session["user_id"] = random.choice(patient_history["profesionales_que_atendieron"])
+        medical_session["nombre_sesion"] = process_data(barnum.create_nouns(), option)
+        medical_session["fecha"] = process_data(randomDate(), option)
+        medical_session["nombre_profesional"] = process_data(names.get_first_name() + " " + names.get_last_name() + " " + names.get_last_name(), option)
+        medical_session["profesion"] = process_data(random.choice(constant.HC_PROFESSIONAL_PRACTITIONERS), option)
+        medical_session["centro_salud"] = process_data(random.choice(constant.HOSPITALS), option)
+        medical_session["user_id"] = process_data(str(random.choice(constant.HC_PROFESSIONAL_ID)), option)
         medical_session["arquetipos"] = []
         patient_history["sesiones_medica"].append(medical_session)
         n_archetype = random.randint(1,5)
@@ -99,14 +129,18 @@ for ehr in range(n_health_records):
             archetype_from_db = archetype_collection.find_one({"text":archetype_list[random.randint(0,n_collections-1)]['text']})
             archetype_from_db.pop('_id')
             archetype_for_session = [{
-                "tipo":1, "clave":archetype_from_db['text'], "valor":archetype_from_db['text']
+                "tipo":process_data("1", option), "clave":process_data(archetype_from_db['text'], option), "valor":process_data(archetype_from_db['text'], option)
             }]
 
             iterateArchetype(archetype_from_db)
             medical_session["arquetipos"].append(archetype_for_session)
     
-    health_records.append(patient_history)
+    if option == 1:
+        health_records.append(patient_history)
+    elif option == 2:
+        result_post = paciente_collection.insert_one(patient_history).inserted_id
     print(str(ehr + 1) + "/" + str(n_health_records))
 
-with open('health_records.json', 'w') as json_file:
-    json.dump(health_records, json_file)
+if option == 1:
+    with open('health_records.json', 'w') as json_file:
+        json.dump(health_records, json_file)
